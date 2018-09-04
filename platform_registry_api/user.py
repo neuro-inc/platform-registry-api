f47c540560f07cdfa13382cc1e67091a64577ce9
Aleksandr Danshyn <dalazx@gmail.com>
2018-09-04 14:06:13 +0300

    Added UserService

---
+++ platform_registry_api/user.py
@@ -0,0 +1,45 @@
+import abc
+from dataclasses import dataclass, field
+
+from aiohttp import BasicAuth
+
+from .config import Config
+
+
+@dataclass
+class User:
+    name: str
+    password: str = field(repr=False)
+
+
+class UserServiceException(Exception):
+    pass
+
+
+class UserService(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    async def get_user_by_name(self, name: str) -> User:
+        pass
+
+    async def get_user_with_credentials(self, credentials: BasicAuth) -> User:
+        user = await self.get_user_by_name(credentials.login)
+        if user.password != credentials.password:
+            raise UserServiceException(
+                f'User "{credentials.login}" was not found')
+        return user
+
+
+class InMemoryUserService(UserService):
+    def __init__(self, config: Config) -> None:
+        self._users = {
+            user.name: user
+            for user in (
+                User(name='neuromation', password=''),  # type: ignore
+            )
+        }
+
+    async def get_user_by_name(self, name: str) -> User:
+        user = self._users.get(name)
+        if not user:
+            raise UserServiceException(f'User "{name}" was not found')
+        return user
