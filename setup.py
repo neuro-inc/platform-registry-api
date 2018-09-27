eb6c78473b44918e9449efe47c15ceee1be1f1df
Aleksandr Danshyn <dalazx@gmail.com>
2018-09-27 14:44:00 +0300

    Added missing dependencies

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ install_requires = (
     'aiodns==1.1.1',
     'aiohttp==3.4.4',
     'aiohttp-remotes==0.1.2',
+    'async-exit-stack==1.0.1',  # backport from 3.7 stdlib
     'cchardet==2.1.1',
     'dataclasses==0.6',  # backport from 3.7 stdlib
+    'neuro_auth_client==0.0.1b1.dev0',
 )
 
 setup(
