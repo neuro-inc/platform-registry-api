61581d63a15c1a015f04da39ba0e4fba7c4f6cf2
Artem Yushkovskiy <artem.yushkovskiy@neuromation.io>
2019-01-16 20:54:53 +0300

    Improve lint & format (#38)

--- platform_registry_api/api.py
+++ platform_registry_api/api.py
@@ -8,8 +8,12 @@ import aiohttp.web
 import aiohttp_remotes
 from aiohttp import BasicAuth, ClientSession
 from aiohttp.web import (
-    Application, HTTPBadRequest, HTTPUnauthorized, Request, Response,
-    StreamResponse
+    Application,
+    HTTPBadRequest,
+    HTTPUnauthorized,
+    Request,
+    Response,
+    StreamResponse,
 )
 from aiohttp_security import check_authorized, check_permission
 from async_exit_stack import AsyncExitStack
@@ -28,13 +32,14 @@ logger = logging.getLogger(__name__)
 class RepoURL:
     # TODO: ClassVar[re.Pattern] in 3.7
     _path_re: ClassVar[Any] = re.compile(
-        r'/v2/(?P<repo>.+)/(?P<path_suffix>(tags|manifests|blobs)/.*)')
+        r"/v2/(?P<repo>.+)/(?P<path_suffix>(tags|manifests|blobs)/.*)"
+    )
 
     repo: str
     url: URL
 
     @classmethod
-    def from_url(cls, url: URL) -> 'RepoURL':
+    def from_url(cls, url: URL) -> "RepoURL":
         # validating the url
         repo, _ = cls._parse(url)
         return cls(repo=repo, url=url)  # type: ignore
@@ -43,35 +48,36 @@ class RepoURL:
     def _parse(cls, url: URL) -> Tuple[str, URL]:
         match = cls._path_re.fullmatch(url.path)
         if not match:
-            raise ValueError(f'unexpected path in a registry URL: {url}')
-        path_suffix = URL.build(
-            path=match.group('path_suffix'), query=url.query)
+            raise ValueError(f"unexpected path in a registry URL: {url}")
+        path_suffix = URL.build(path=match.group("path_suffix"), query=url.query)
         assert not path_suffix.is_absolute()
-        return match.group('repo'), path_suffix
+        return match.group("repo"), path_suffix
 
-    def with_repo(self, repo: str) -> 'RepoURL':
+    def with_repo(self, repo: str) -> "RepoURL":
         _, url_suffix = self._parse(self.url)
-        rel_url = URL(f'/v2/{repo}/').join(url_suffix)
+        rel_url = URL(f"/v2/{repo}/").join(url_suffix)
         url = self.url.join(rel_url)
         # TODO: dataclasses.replace turns out out be buggy :D
         return self.__class__(repo=repo, url=url)
 
-    def with_origin(self, origin_url: URL) -> 'RepoURL':
+    def with_origin(self, origin_url: URL) -> "RepoURL":
         url = origin_url.join(self.url.relative())
         return self.__class__(repo=self.repo, url=url)
 
 
 class URLFactory:
     def __init__(
-            self, registry_endpoint_url: URL, upstream_endpoint_url: URL,
-            upstream_project: str) -> None:
+        self,
+        registry_endpoint_url: URL,
+        upstream_endpoint_url: URL,
+        upstream_project: str,
+    ) -> None:
         self._registry_endpoint_url = registry_endpoint_url
         self._upstream_endpoint_url = upstream_endpoint_url
         self._upstream_project = upstream_project
 
     @classmethod
-    def from_config(
-            cls, registry_endpoint_url: URL, config: Config) -> 'URLFactory':
+    def from_config(cls, registry_endpoint_url: URL, config: Config) -> "URLFactory":
         return cls(
             registry_endpoint_url=registry_endpoint_url,
             upstream_endpoint_url=config.upstream_registry.endpoint_url,
@@ -79,46 +85,40 @@ class URLFactory:
         )
 
     def create_registry_version_check_url(self) -> URL:
-        return self._upstream_endpoint_url.with_path('/v2/')
+        return self._upstream_endpoint_url.with_path("/v2/")
 
     def create_upstream_repo_url(self, registry_url: RepoURL) -> RepoURL:
-        repo = f'{self._upstream_project}/{registry_url.repo}'
-        return (
-            registry_url
-            .with_repo(repo)
-            .with_origin(self._upstream_endpoint_url))
+        repo = f"{self._upstream_project}/{registry_url.repo}"
+        return registry_url.with_repo(repo).with_origin(self._upstream_endpoint_url)
 
     def create_registry_repo_url(self, upstream_url: RepoURL) -> RepoURL:
         upstream_repo = upstream_url.repo
         try:
-            upstream_project, repo = upstream_repo.split('/', 1)
+            upstream_project, repo = upstream_repo.split("/", 1)
         except ValueError:
-            upstream_project, repo = '', upstream_repo
+            upstream_project, repo = "", upstream_repo
         if upstream_project != self._upstream_project:
             raise ValueError(
                 f'Upstream project "{upstream_project}" does not match '
-                f'the one configured "{self._upstream_project}"')
-        return (
-            upstream_url
-            .with_repo(repo)
-            .with_origin(self._registry_endpoint_url))
+                f'the one configured "{self._upstream_project}"'
+            )
+        return upstream_url.with_repo(repo).with_origin(self._registry_endpoint_url)
 
 
 class UpstreamTokenManager:
     def __init__(
-            self, client: ClientSession,
-            registry_config: UpstreamRegistryConfig) -> None:
+        self, client: ClientSession, registry_config: UpstreamRegistryConfig
+    ) -> None:
         self._client = client
         self._registry_config = registry_config
 
         self._auth = BasicAuth(
             login=self._registry_config.token_endpoint_username,
-            password=self._registry_config.token_endpoint_password)
+            password=self._registry_config.token_endpoint_password,
+        )
 
-        self._base_url = (
-            self._registry_config.token_endpoint_url.with_query({
-                'service': self._registry_config.token_service,
-            })
+        self._base_url = self._registry_config.token_endpoint_url.with_query(
+            {"service": self._registry_config.token_service}
         )
 
     async def _request(self, url: URL) -> str:
@@ -126,22 +126,18 @@ class UpstreamTokenManager:
             # TODO: check the status code
             # TODO: raise exceptions
             payload = await response.json()
-            return payload['token']
+            return payload["token"]
 
     async def get_token_without_scope(self) -> str:
         url = self._base_url
         return await self._request(url)
 
     async def get_token_for_catalog(self) -> str:
-        url = self._base_url.update_query({
-            'scope': 'registry:catalog:*',
-        })
+        url = self._base_url.update_query({"scope": "registry:catalog:*"})
         return await self._request(url)
 
     async def get_token_for_repo(self, repo: str) -> str:
-        url = self._base_url.update_query({
-            'scope': f'repository:{repo}:*',
-        })
+        url = self._base_url.update_query({"scope": f"repository:{repo}:*"})
         return await self._request(url)
 
 
@@ -153,20 +149,24 @@ class V2Handler:
 
     @property
     def _registry_client(self) -> aiohttp.ClientSession:
-        return self._app['registry_client']
+        return self._app["registry_client"]
 
     @property
     def _upstream_token_manager(self) -> UpstreamTokenManager:
-        return self._app['upstream_token_manager']
+        return self._app["upstream_token_manager"]
 
     def register(self, app):
-        app.add_routes((
-            aiohttp.web.get('/', self.handle_version_check),
-            aiohttp.web.get('/_catalog', self.handle_catalog),
-            aiohttp.web.route(
-                '*', r'/{repo:.+}/{path_suffix:(tags|manifests|blobs)/.*}',
-                self.handle),
-        ))
+        app.add_routes(
+            (
+                aiohttp.web.get("/", self.handle_version_check),
+                aiohttp.web.get("/_catalog", self.handle_catalog),
+                aiohttp.web.route(
+                    "*",
+                    r"/{repo:.+}/{path_suffix:(tags|manifests|blobs)/.*}",
+                    self.handle,
+                ),
+            )
+        )
 
     def _create_url_factory(self, request: Request) -> URLFactory:
         return URLFactory.from_config(
@@ -183,14 +183,13 @@ class V2Handler:
         return User(name=user_name)
 
     def _raise_unauthorized(self) -> None:
-        raise HTTPUnauthorized(headers={
-            'WWW-Authenticate': f'Basic realm="{self._config.server.name}"',
-        })
+        raise HTTPUnauthorized(
+            headers={"WWW-Authenticate": f'Basic realm="{self._config.server.name}"'}
+        )
 
     async def handle_version_check(self, request: Request) -> StreamResponse:
         # TODO: prevent leaking sensitive headers
-        logger.debug(
-            'registry request: %s; headers: %s', request, request.headers)
+        logger.debug("registry request: %s; headers: %s", request, request.headers)
 
         await self._get_user_from_request(request)
 
@@ -198,7 +197,8 @@ class V2Handler:
         url = url_factory.create_registry_version_check_url()
         token = await self._upstream_token_manager.get_token_without_scope()
         return await self._proxy_request(
-            request, url_factory=url_factory, url=url, token=token)
+            request, url_factory=url_factory, url=url, token=token
+        )
 
     async def handle_catalog(self, request: Request) -> StreamResponse:
         # TODO: compose proper payload
@@ -207,39 +207,40 @@ class V2Handler:
 
     async def handle(self, request: Request) -> StreamResponse:
         # TODO: prevent leaking sensitive headers
-        logger.debug(
-            'registry request: %s; headers: %s', request, request.headers)
+        logger.debug("registry request: %s; headers: %s", request, request.headers)
 
         registry_repo_url = RepoURL.from_url(request.url)
 
         await self._check_user_permissions(request, registry_repo_url.repo)
 
         url_factory = self._create_url_factory(request)
-        upstream_repo_url = url_factory.create_upstream_repo_url(
-            registry_repo_url)
+        upstream_repo_url = url_factory.create_upstream_repo_url(registry_repo_url)
 
         logger.info(
-            'converted registry repo URL to upstream repo URL: %s -> %s',
-            registry_repo_url, upstream_repo_url)
+            "converted registry repo URL to upstream repo URL: %s -> %s",
+            registry_repo_url,
+            upstream_repo_url,
+        )
 
         token = await self._upstream_token_manager.get_token_for_repo(
-            upstream_repo_url.repo)
+            upstream_repo_url.repo
+        )
 
         return await self._proxy_request(
-            request, url_factory=url_factory,
-            url=upstream_repo_url.url, token=token)
+            request, url_factory=url_factory, url=upstream_repo_url.url, token=token
+        )
 
     def _is_pull_request(self, request: Request) -> bool:
-        return request.method in ('HEAD', 'GET')
+        return request.method in ("HEAD", "GET")
 
     async def _check_user_permissions(self, request, repo: str) -> None:
-        uri = 'image://' + repo
+        uri = "image://" + repo
         if self._is_pull_request(request):
-            action = 'read'
+            action = "read"
         else:  # POST, PUT, PATCH, DELETE
-            action = 'write'
+            action = "write"
         permission = Permission(uri=uri, action=action)
-        logger.info(f'Checking {permission}')
+        logger.info(f"Checking {permission}")
         try:
             await check_permission(request, action, [permission])
         except HTTPUnauthorized:
@@ -250,9 +251,7 @@ class V2Handler:
     ) -> aiohttp.ClientTimeout:
         sock_read_timeout_s = None
         if self._is_pull_request(request):
-            sock_read_timeout_s = (
-                self._upstream_registry_config.sock_read_timeout_s
-            )
+            sock_read_timeout_s = self._upstream_registry_config.sock_read_timeout_s
         return aiohttp.ClientTimeout(
             total=None,
             connect=None,
@@ -261,34 +260,35 @@ class V2Handler:
         )
 
     async def _proxy_request(
-            self, request: Request, url_factory: URLFactory, url: URL,
-            token: str) -> StreamResponse:
-        request_headers = self._prepare_request_headers(
-            request.headers, token=token)
+        self, request: Request, url_factory: URLFactory, url: URL, token: str
+    ) -> StreamResponse:
+        request_headers = self._prepare_request_headers(request.headers, token=token)
 
         timeout = self._create_registry_client_timeout(request)
 
         async with self._registry_client.request(
-                method=request.method,
-                url=url,
-                headers=request_headers,
-                skip_auto_headers=('Content-Type',),
-                data=request.content.iter_any(),
-                timeout=timeout) as client_response:
+            method=request.method,
+            url=url,
+            headers=request_headers,
+            skip_auto_headers=("Content-Type",),
+            data=request.content.iter_any(),
+            timeout=timeout,
+        ) as client_response:
 
-            logger.debug('upstream response: %s', client_response)
+            logger.debug("upstream response: %s", client_response)
 
             response_headers = self._prepare_response_headers(
-                client_response.headers, url_factory)
+                client_response.headers, url_factory
+            )
             response = aiohttp.web.StreamResponse(
-                status=client_response.status,
-                headers=response_headers)
+                status=client_response.status, headers=response_headers
+            )
 
             await response.prepare(request)
 
             logger.debug(
-                'registry response: %s; headers: %s',
-                response, response.headers)
+                "registry response: %s; headers: %s", response, response.headers
+            )
 
             async for chunk in client_response.content.iter_any():
                 await response.write(chunk)
@@ -297,36 +297,38 @@ class V2Handler:
             return response
 
     def _prepare_request_headers(
-            self, headers: CIMultiDictProxy, token: str) -> CIMultiDict:
+        self, headers: CIMultiDictProxy, token: str
+    ) -> CIMultiDict:
         request_headers: CIMultiDict = headers.copy()  # type: ignore
 
-        for name in ('Host', 'Transfer-Encoding', 'Connection'):
+        for name in ("Host", "Transfer-Encoding", "Connection"):
             request_headers.pop(name, None)
 
-        request_headers['Authorization'] = f'Bearer {token}'
+        request_headers["Authorization"] = f"Bearer {token}"
         return request_headers
 
     def _prepare_response_headers(
-            self, headers: CIMultiDictProxy, url_factory: URLFactory
-            ) -> CIMultiDict:
+        self, headers: CIMultiDictProxy, url_factory: URLFactory
+    ) -> CIMultiDict:
         response_headers: CIMultiDict = headers.copy()  # type: ignore
 
-        for name in ('Transfer-Encoding', 'Content-Encoding', 'Connection'):
+        for name in ("Transfer-Encoding", "Content-Encoding", "Connection"):
             response_headers.pop(name, None)
 
-        if 'Location' in response_headers:
-            response_headers['Location'] = self._convert_location_header(
-                response_headers['Location'], url_factory)
+        if "Location" in response_headers:
+            response_headers["Location"] = self._convert_location_header(
+                response_headers["Location"], url_factory
+            )
         return response_headers
 
-    def _convert_location_header(
-            self, url_str: str, url_factory: URLFactory) -> str:
+    def _convert_location_header(self, url_str: str, url_factory: URLFactory) -> str:
         upstream_repo_url = RepoURL.from_url(URL(url_str))
-        registry_repo_url = url_factory.create_registry_repo_url(
-            upstream_repo_url)
+        registry_repo_url = url_factory.create_registry_repo_url(upstream_repo_url)
         logger.info(
-            'converted upstream repo URL to registry repo URL: %s -> %s',
-            upstream_repo_url, registry_repo_url)
+            "converted upstream repo URL to registry repo URL: %s -> %s",
+            upstream_repo_url,
+            registry_repo_url,
+        )
         return str(registry_repo_url.url)
 
 
@@ -339,34 +341,30 @@ async def create_app(config: Config) -> aiohttp.web.Application:
         async with AsyncExitStack() as exit_stack:
 
             async def on_request_redirect(session, ctx, params):
-                logger.debug('upstream redirect response: %s', params.response)
+                logger.debug("upstream redirect response: %s", params.response)
 
             trace_config = aiohttp.TraceConfig()
             trace_config.on_request_redirect.append(on_request_redirect)
 
-            logger.info('Initializing Registry Client Session')
+            logger.info("Initializing Registry Client Session")
 
             session = await exit_stack.enter_async_context(
-                aiohttp.ClientSession(
-                    trace_configs=[trace_config],
+                aiohttp.ClientSession(trace_configs=[trace_config])
+            )
+
+            app["v2_app"]["registry_client"] = session
+            app["v2_app"]["upstream_token_manager"] = UpstreamTokenManager(
+                client=session, registry_config=config.upstream_registry
+            )
+
+            auth_client = await exit_stack.enter_async_context(
+                AuthClient(
+                    url=config.auth.server_endpoint_url, token=config.auth.service_token
                 )
             )
 
-            app['v2_app']['registry_client'] = session
-            app['v2_app']['upstream_token_manager'] = UpstreamTokenManager(
-                client=session,
-                registry_config=config.upstream_registry,
-            )
-
-            auth_client = await exit_stack.enter_async_context(AuthClient(
-                url=config.auth.server_endpoint_url,
-                token=config.auth.service_token,
-            ))
-
             await setup_security(
-                app=app,
-                auth_client=auth_client,
-                auth_scheme=AuthScheme.BASIC
+                app=app, auth_client=auth_client, auth_scheme=AuthScheme.BASIC
             )
 
             yield
@@ -377,8 +375,8 @@ async def create_app(config: Config) -> aiohttp.web.Application:
     v2_handler = V2Handler(app=v2_app, config=config)
     v2_handler.register(v2_app)
 
-    app['v2_app'] = v2_app
-    app.add_subapp('/v2', v2_app)
+    app["v2_app"] = v2_app
+    app.add_subapp("/v2", v2_app)
     return app
 
 
@@ -386,7 +384,8 @@ def init_logging():
     logging.basicConfig(
         # TODO (A Danshyn 06/01/18): expose in the Config
         level=logging.DEBUG,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
 
 
 def main():
@@ -395,8 +394,7 @@ def main():
     loop = asyncio.get_event_loop()
 
     config = EnvironConfigFactory().create()
-    logger.info('Loaded config: %r', config)
+    logger.info("Loaded config: %r", config)
 
     app = loop.run_until_complete(create_app(config))
-    aiohttp.web.run_app(
-        app, host=config.server.host, port=config.server.port)
+    aiohttp.web.run_app(app, host=config.server.host, port=config.server.port)
--- platform_registry_api/config.py
+++ platform_registry_api/config.py
@@ -7,9 +7,9 @@ from yarl import URL
 
 @dataclass(frozen=True)
 class ServerConfig:
-    host: str = '0.0.0.0'
+    host: str = "0.0.0.0"
     port: int = 8080
-    name: str = 'Docker Registry'
+    name: str = "Docker Registry"
 
 
 @dataclass(frozen=True)
@@ -28,8 +28,8 @@ class UpstreamRegistryConfig:
     token_endpoint_username: str = field(repr=False)
     token_endpoint_password: str = field(repr=False)
 
-    sock_connect_timeout_s: Optional[float] = 30.
-    sock_read_timeout_s: Optional[float] = 30.
+    sock_connect_timeout_s: Optional[float] = 30.0
+    sock_read_timeout_s: Optional[float] = 30.0
 
 
 @dataclass(frozen=True)
@@ -44,19 +44,16 @@ class EnvironConfigFactory:
         self._environ = environ or os.environ
 
     def create_server(self) -> ServerConfig:
-        port = int(os.environ.get('NP_REGISTRY_API_PORT', ServerConfig.port))
+        port = int(os.environ.get("NP_REGISTRY_API_PORT", ServerConfig.port))
         return ServerConfig(port=port)  # type: ignore
 
     def create_upstream_registry(self) -> UpstreamRegistryConfig:
-        endpoint_url = URL(self._environ['NP_REGISTRY_UPSTREAM_URL'])
-        project = self._environ['NP_REGISTRY_UPSTREAM_PROJECT']
-        token_endpoint_url = URL(
-            self._environ['NP_REGISTRY_UPSTREAM_TOKEN_URL'])
-        token_service = self._environ['NP_REGISTRY_UPSTREAM_TOKEN_SERVICE']
-        token_endpoint_username = self._environ[
-            'NP_REGISTRY_UPSTREAM_TOKEN_USERNAME']
-        token_endpoint_password = self._environ[
-            'NP_REGISTRY_UPSTREAM_TOKEN_PASSWORD']
+        endpoint_url = URL(self._environ["NP_REGISTRY_UPSTREAM_URL"])
+        project = self._environ["NP_REGISTRY_UPSTREAM_PROJECT"]
+        token_endpoint_url = URL(self._environ["NP_REGISTRY_UPSTREAM_TOKEN_URL"])
+        token_service = self._environ["NP_REGISTRY_UPSTREAM_TOKEN_SERVICE"]
+        token_endpoint_username = self._environ["NP_REGISTRY_UPSTREAM_TOKEN_USERNAME"]
+        token_endpoint_password = self._environ["NP_REGISTRY_UPSTREAM_TOKEN_PASSWORD"]
         return UpstreamRegistryConfig(  # type: ignore
             endpoint_url=endpoint_url,
             project=project,
@@ -64,15 +61,12 @@ class EnvironConfigFactory:
             token_service=token_service,
             token_endpoint_username=token_endpoint_username,
             token_endpoint_password=token_endpoint_password,
-            )
+        )
 
     def create_auth(self) -> AuthConfig:
-        url = URL(self._environ['NP_REGISTRY_AUTH_URL'])
-        token = self._environ['NP_REGISTRY_AUTH_TOKEN']
-        return AuthConfig(  # type: ignore
-            server_endpoint_url=url,
-            service_token=token,
-        )
+        url = URL(self._environ["NP_REGISTRY_AUTH_URL"])
+        token = self._environ["NP_REGISTRY_AUTH_TOKEN"]
+        return AuthConfig(server_endpoint_url=url, service_token=token)  # type: ignore
 
     def create(self) -> Config:
         server_config = self.create_server()
