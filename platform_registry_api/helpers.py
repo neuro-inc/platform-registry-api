95de17482fdb3e5d67ea208ac7f975c87f7b427e
Artem Yushkovskiy <ajuszkowski@ya.ru>
2019-01-16 20:58:13 +0300

    review quickfix

--- platform_registry_api/helpers.py
+++ platform_registry_api/helpers.py
@@ -5,11 +5,8 @@ def check_image_catalog_permission(
         image_name_and_tag: str,
         tree: ClientSubTreeViewRoot
 ) -> bool:
-    # The permission tree contains the tagged image name
-    # for example, "username/image/name/with/slashes:tag"
     parts = image_name_and_tag.split('/')
     node = tree.sub_tree
-    # colon ":" is allowed only at leafs (docker image name format)
     for part in parts:
         # if permission is not "list", we can make decision already
         if node.action != 'list':
