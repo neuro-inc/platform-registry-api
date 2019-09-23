30d104eedaff0921702a75898f6f4854dca82734
Serhiy Storchaka <storchaka@gmail.com>
2019-09-23 16:17:00 +0300

    Use ClientAccessSubTreeView.can_read().

--- platform_registry_api/helpers.py
+++ platform_registry_api/helpers.py
@@ -6,12 +6,14 @@ def check_image_catalog_permission(
 ) -> bool:
     parts = image_name_and_tag.split("/")
     node = tree.sub_tree
+    # if permission is not "list", we can make decision already
+    if node.can_read():
+        return True
     for part in parts:
-        # if permission is not "list", we can make decision already
-        if node.action != "list":
-            break
         child = node.children.get(part)
         if child is None:
             break
-        node = node.children[part]
-    return node.action not in {"deny", "list"}
+        node = child
+        if node.can_read():
+            return True
+    return False
