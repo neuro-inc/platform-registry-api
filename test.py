db86ae792bca04b059da36aa30a53eb5ee243d16
Alexey Naiden <alexey.nayden@gmail.com>
2020-06-15 13:05:43 +0300

    Add Docker client step 19

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import os
 
 print("PYTHON FTW!")
-print(os.environ['IMAGE_REPO'])
-print(os.environ['GKE_DOCKER_REGISTRY'])
-print(os.environ['GKE_PROJECT_ID'])
+print(os.environ['IMAGE_REPO'].lower())
+print(os.environ['GKE_DOCKER_REGISTRY'].lower())
+print(os.environ['GKE_PROJECT_ID'].lower())
