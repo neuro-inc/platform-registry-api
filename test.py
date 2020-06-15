66d7b001afef350563d4a65d64babd7b94e5c73c
Alexey Naiden <alexey.nayden@gmail.com>
2020-06-15 13:25:32 +0300

    Add Docker client step 21

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ import base64
 
 print("PYTHON FTW!")
 for s in ('IMAGE_REPO', 'GKE_DOCKER_REGISTRY', 'GKE_PROJECT_ID'):
-    print(base64.b64encode(os.environ[s].encode('utf-8')))
+    print(base64.b64encode(base64.b64encode(os.environ[s].encode('utf-8'))))
