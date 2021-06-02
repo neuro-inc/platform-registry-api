b92b347b88a1265408bc299f612b2ad84f121c45
Andrew Svetlov <andrew.svetlov@gmail.com>
2021-06-02 23:55:57 +0300

    Bump to python 3.8 (#337)

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     packages=find_packages(),
     setup_requires=setup_requires,
     install_requires=install_requires,
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     entry_points={
         "console_scripts": "platform-registry-api=platform_registry_api.api:main"
     },
