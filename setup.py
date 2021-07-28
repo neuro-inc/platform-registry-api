5db7d1615ede78083754fb5ebd3bcaa3a0c018f9
Alexey Naiden <17383+anayden@users.noreply.github.com>
2021-07-28 12:26:01 +0300

    An/rollback aiohttp version (#375)
    * Rollback aiohttp version
    
    * Play with dependencies

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ install_requires = (
     "aiohttp-remotes==1.0.0",
     "cchardet==2.1.7",
     "iso8601==0.1.16",
-    "neuro_auth_client==21.6.15",
-    "uvloop==0.15.3",
+    "neuro_auth_client==21.7.27",
+    "uvloop==0.14.0",
     "aiobotocore==1.3.3",
     "platform-logging==21.7.27",
     "trafaret==2.1.0",
     "aiozipkin==1.1.0",
     "yarl==1.6.3",
-    "sentry-sdk==1.3.0",
+    "sentry-sdk==1.3.1",
 )
 
 setup(
