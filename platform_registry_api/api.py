1aef9937922357185ae4b87283c856fce601c8e5
Andrew Svetlov <andrew.svetlov@gmail.com>
2021-07-30 12:50:17 +0300

    Don't send RepositoryNotFoundException event to Sentry

--- platform_registry_api/api.py
+++ platform_registry_api/api.py
@@ -925,6 +925,7 @@ def setup_tracing(config: Config) -> None:
             app_name=config.sentry.app_name,
             cluster_name=config.sentry.cluster_name,
             sample_rate=config.sentry.sample_rate,
+            exclude=[aiobotocore.RepositoryNotFoundException],
         )
 
 
