a34d593b6ab490359ce09cae2b1572e16015fd11
Ivan Zubenko <vanya.zubenko@gmail.com>
2024-11-20 22:11:21 +0200

    upgrade neuro-logging to 24.4.0 (#888)

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -4,11 +4,9 @@ from platform_registry_api.config import (
     AuthConfig,
     Config,
     EnvironConfigFactory,
-    SentryConfig,
     ServerConfig,
     UpstreamRegistryConfig,
     UpstreamType,
-    ZipkinConfig,
 )
 
 
@@ -64,9 +62,6 @@ class TestEnvironConfigFactory:
             "NP_REGISTRY_UPSTREAM_TOKEN_REGISTRY_SCOPE": "",
             "NP_REGISTRY_UPSTREAM_TOKEN_REPO_SCOPE_ACTIONS": "push,pull",
             "NP_CLUSTER_NAME": "test-cluster",
-            "NP_ZIPKIN_URL": "http://zipkin.io:9411/",
-            "NP_SENTRY_DSN": "https://sentry",
-            "NP_SENTRY_CLUSTER_NAME": "test",
         }
         config = EnvironConfigFactory(environ=environ).create()
         assert config == Config(
@@ -88,8 +83,6 @@ class TestEnvironConfigFactory:
                 service_token="test_auth_token",
             ),
             cluster_name="test-cluster",
-            zipkin=ZipkinConfig(URL("http://zipkin.io:9411/")),
-            sentry=SentryConfig(dsn=URL("https://sentry"), cluster_name="test"),
         )
         assert config.upstream_registry.is_oauth
 
@@ -179,56 +172,3 @@ class TestEnvironConfigFactory:
         )
         assert config.upstream_registry.is_basic
         assert not config.upstream_registry.is_oauth
-
-    def test_create_zipkin_none(self) -> None:
-        result = EnvironConfigFactory({}).create_zipkin()
-
-        assert result is None
-
-    def test_create_zipkin_default(self) -> None:
-        env = {"NP_ZIPKIN_URL": "https://zipkin:9411"}
-        result = EnvironConfigFactory(env).create_zipkin()
-
-        assert result == ZipkinConfig(url=URL("https://zipkin:9411"))
-
-    def test_create_zipkin_custom(self) -> None:
-        env = {
-            "NP_ZIPKIN_URL": "https://zipkin:9411",
-            "NP_ZIPKIN_APP_NAME": "api",
-            "NP_ZIPKIN_SAMPLE_RATE": "1",
-        }
-        result = EnvironConfigFactory(env).create_zipkin()
-
-        assert result == ZipkinConfig(
-            url=URL("https://zipkin:9411"), app_name="api", sample_rate=1
-        )
-
-    def test_create_sentry_none(self) -> None:
-        result = EnvironConfigFactory({}).create_sentry()
-
-        assert result is None
-
-    def test_create_sentry_default(self) -> None:
-        env = {
-            "NP_SENTRY_DSN": "https://sentry",
-            "NP_SENTRY_CLUSTER_NAME": "test",
-        }
-        result = EnvironConfigFactory(env).create_sentry()
-
-        assert result == SentryConfig(dsn=URL("https://sentry"), cluster_name="test")
-
-    def test_create_sentry_custom(self) -> None:
-        env = {
-            "NP_SENTRY_DSN": "https://sentry",
-            "NP_SENTRY_APP_NAME": "api",
-            "NP_SENTRY_CLUSTER_NAME": "test",
-            "NP_SENTRY_SAMPLE_RATE": "1",
-        }
-        result = EnvironConfigFactory(env).create_sentry()
-
-        assert result == SentryConfig(
-            dsn=URL("https://sentry"),
-            app_name="api",
-            cluster_name="test",
-            sample_rate=1,
-        )
