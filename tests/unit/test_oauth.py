6473b68bb93717fed969649b74d606af76a4d9a8
Yevhenii Semendiak <semendyak@gmail.com>
2026-06-17 18:43:03 +0300

    fix test

--- tests/unit/test_oauth.py
+++ tests/unit/test_oauth.py
@@ -14,12 +14,13 @@ class TestOAuthToken:
     def test_create_from_payload_access_token(
         self, monkeypatch: pytest.MonkeyPatch
     ) -> None:
+        secret = "mysupeduperrsecretiwilltellnoone"
         monkeypatch.setattr(time, "time", lambda: 1560000000.0)
-        access_token = jwt.encode({"sub": "without-exp"}, "secret", algorithm="HS256")
+        access_token = jwt.encode({"sub": "without-exp"}, secret, algorithm="HS256")
         token = OAuthToken.create_from_payload({"access_token": access_token})
         assert token == OAuthToken(access_token=access_token, expires_at=1560000045.0)
 
-        access_token = jwt.encode({"exp": 1560000060}, "secret", algorithm="HS256")
+        access_token = jwt.encode({"exp": 1560000060}, secret, algorithm="HS256")
         token = OAuthToken.create_from_payload({"access_token": access_token})
         assert token == OAuthToken(access_token=access_token, expires_at=1560000045.0)
 
